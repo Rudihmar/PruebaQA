834209a991ed625b01493875cf8c0a731859db62
Rudihmar Suarez <rudihmar@gmail.com>
2023-01-10 16:58:15 -0300

    edit user

--- PruebaEquifax.py
+++ PruebaEquifax.py
@@ -55,3 +55,22 @@ submit = driver.find_element(By.XPATH,'/html[1]/body[1]/div[4]/div[1]/div[1]/div
 submit.click()
 driver.execute_script("window.scrollTo(0, window.scrollY + 50)")
 time.sleep(2)
+
+'''click editar'''
+editar = driver.find_element(By.CSS_SELECTOR,'div.body-height:nth-child(2) div.container.playgound-body div.row div.col-12.mt-4.col-md-6:nth-child(2) div.web-tables-wrapper:nth-child(2) div.ReactTable.-striped.-highlight div.rt-table div.rt-tbody div.rt-tr-group:nth-child(4) div.rt-tr.-even div.rt-td:nth-child(7) div.action-buttons span.mr-2:nth-child(1) > svg:nth-child(1)')
+editar.click()
+print('Editar usuario')
+time.sleep(10)
+
+'''editar edad'''
+age1 = driver.find_element(By.XPATH,'/html[1]/body[1]/div[4]/div[1]/div[1]/div[2]/form[1]/div[4]/div[2]/input[1]')
+age1.click()
+age1.clear()
+time.sleep(2)
+age1.send_keys('25')
+time.sleep(2)
+
+'''click submit'''
+add = driver.find_element(By.XPATH,'/html[1]/body[1]/div[4]/div[1]/div[1]/div[2]/form[1]/div[7]/div[1]/button[1]')
+add.click()
+time.sleep(2)
